58f5ff18c3354e6dcb008f7b83f3f3b85fe86cfb
samm329-ui <jishudas329@gmail.com>
2026-04-16 00:53:22 +0530

    Update next.config.ts and add .npmrc for video-editor

--- video-editor/next.config.ts
+++ video-editor/next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	reactStrictMode: false,
 	typescript: {
 		ignoreBuildErrors: true,
 	},
-	turbopack: false,
 };
 
 export default nextConfig;
